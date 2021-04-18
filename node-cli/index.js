744fe105cf23a43b6dcd59485e16ca77934ab9a8
eternal7s <eternal7s@naver.com>
2021-04-18 23:01:32 +0900

    node cli 프로그래밍

---
+++ node-cli/index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+// console.log('helo CLI', process.argv);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.clear();
+const answerCallback = (answer) => {
+    if (answer === 'y') {
+        console.log('감사합니다.!');
+        rl.close();
+    } else if (answer === 'n') {
+        console.log('죄송힙니다!');
+        rl.close();
+    } else {
+        console.clear();
+        console.log('y 또는 n만 입력하세요.')
+        rl.question('예제가 재미있습니까? (y/n) ', answerCallback);
+    }
+};
+rl.question('예제가 재미있습니까? (y/n) ', answerCallback);
+
+
+
+
